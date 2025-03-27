4ace5000c025bc48d16fa31ae6b6a11d52774de6
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2025-03-27 17:43:05 +0530

    Comment out HeroScrollDemo component in Home page to streamline layout and improve performance.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import GlowingGridBackground from "@/components/grid/grid-bg";
-import { HeroScrollDemo } from "@/components/landing-page/scroll";
+// import { HeroScrollDemo } from "@/components/landing-page/scroll";
 import Herosection from "../components/landing-page/herosection";
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="fixed inset-0 -z-10 bg-[#040609]">
         <GlowingGridBackground />
       </div>
-      
+
       <Herosection />
       {/* <HeroScrollDemo/> */}
     </>
