140fb9f48c7e3b323bf6b2e4cd25245b0478fdd7
Santanu Dutta <155149934+santanu4246@users.noreply.github.com>
2025-03-27 17:26:27 +0530

    Add HeroSection and HeroScrollDemo components to the home page for enhanced user engagement

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
       <div className="fixed inset-0 -z-10 bg-[#040609]">
         <GlowingGridBackground />
       </div>
+      
       <Herosection />
       <HeroScrollDemo/>
     </>
